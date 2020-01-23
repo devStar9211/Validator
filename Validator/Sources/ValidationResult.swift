1e9bd56a5730a82edb82ac423d92be37930a37aa
devStar9211 <32103851+devStar9211@users.noreply.github.com>
2020-01-23 21:33:23 +0700

    add merge function

--- Validator/Sources/ValidationResult.swift
+++ Validator/Sources/ValidationResult.swift
@@ -10,6 +10,66 @@ public enum ValidationResult {
         
         return self == .valid
     }
+    
+    /**
+     
+     Merges the receiving validation rule with another.
+     
+     ```
+     .valid.merge(.valid) // = .valid
+     .valid.merge(.invalid([err])) // = .invalid([err])
+     .invalid([err1]).merge(.invalid([err2]) // = .invalid([err1, err2])
+     ```
+     
+     - Parameters:
+        - result: The result to merge into the receiver.
+     
+     - Returns:
+     Merged validation result.
+     
+     */
+    public func merge(with result: ValidationResult) -> ValidationResult {
+        switch self {
+        case .valid: return result
+        case .invalid(let errorMessages):
+            switch result {
+            case .valid:
+                return self
+            case .invalid(let errorMessagesAnother):
+                return .invalid([errorMessages, errorMessagesAnother].flatMap { $0 })
+            }
+        }
+    }
+    
+    /**
+     
+     Merges the receiving validation rule with multiple others.
+     
+     - Parameters:
+        - results: The results to merge the receiver.
+     
+     - Returns:
+     Merged validation result.
+     
+     */
+    public func merge(with results: [ValidationResult]) -> ValidationResult {
+        return results.reduce(self) { return $0.merge(with: $1) }
+    }
+    
+    /**
+     
+     Merges multiple validation rules together.
+     
+     - Parameters:
+        - results: The results to merge.
+     
+     - Returns:
+     Merged validation result.
+     
+     */
+    public static func merge(results: [ValidationResult]) -> ValidationResult {
+        return ValidationResult.valid.merge(with: results)
+    }
 }
 
 extension ValidationResult: Equatable {
